d8e5cc9813c8539d865bb237a854d544d172b994
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-01-21 22:22:29 +0600

    first commit

---
+++ 2/Boolean.py
@@ -0,0 +1,18 @@
+# 1
+# 10 > 9 => True
+
+
+# 2
+# 10 == 9 => False
+
+
+# 3
+# 10 < 9 False
+
+
+# 4
+# bool("abc") => True
+
+
+# 5
+# bool(0) => False
---
+++ 3/Inheritance.py
@@ -0,0 +1,26 @@
+# 1
+# class Student(Person):
+
+
+# 2
+# class Person:
+#   def __init__(self, fname):
+#     self.firstname = fname
+
+#   def printname(self):
+#     print(self.firstname)
+
+# class Student(Person):
+#   pass
+
+# x = Student("Mike")
+# x.printname()
+
+
+
+
+
+
+# class Student(Person):
+#   def __init__(self, fname, lname):
+#     super().__init__(fname, lname)
