401769bec7b7975a56fa4b244d6dbb88a59c7f07
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-03-25 18:36:41 +0500

    lab 7

--- 7/circle.py
+++ 7/circle.py
@@ -1,72 +1,34 @@
 import pygame
-import math
+from sys import exit
 
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
 
-def create_background(width, height):
-    colors = [(255, 255, 255), (212, 212, 212)]
-    background = pygame.Surface((width, height))
-    tile_width = 20
-    y = 0
-    while y < height:
-            x = 0
-            while x < width:
-                    row = y // tile_width
-                    col = x // tile_width
-                    pygame.draw.rect(
-                            background, 
-                            colors[(row + col) % 2],
-                            pygame.Rect(x, y, tile_width, tile_width))
-                    x += tile_width
-            y += tile_width
-    return background
-    
+x, y = 400, 400
+delta = 0
 
-def is_trying_to_quit(event):
-        pressed_keys = pygame.key.get_pressed()
-        alt_pressed = pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT]
-        x_button = event.type == pygame.QUIT
-        altF4 = alt_pressed and event.type == pygame.KEYDOWN and event.key == pygame.K_F4
-        escape = event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-        return x_button or altF4 or escape
+while True:
+    pressed = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pressed[pygame.K_ESCAPE]:
+            pygame.quit()
+            exit()
 
+    if pressed[pygame.K_UP] and y >= 27:
+        y -= 20
+    if pressed[pygame.K_DOWN] and y <= 765.5:
+        y += 20
+    if pressed[pygame.K_LEFT] and x >= 27:
+        x -= 20
+    if pressed[pygame.K_RIGHT] and x <= 765.5:
+        x += 20
 
-def run_demos(width, height, fps):
-        pygame.init()
-        screen = pygame.display.set_mode((width, height))
-        pygame.display.set_caption('press space to see next demo')
-        background = create_background(width, height)
-        clock = pygame.time.Clock()
-        demos = [
-                do_circle_demo
-                ]
-        the_world_is_a_happy_place = 0
-        while True:
-                the_world_is_a_happy_place += 1
-                for event in pygame.event.get():
-                        if is_trying_to_quit(event):
-                                return
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                                demos = demos[1:]
-                screen.blit(background, (0, 0))
-                if len(demos) == 0:
-                        return
-                demos[0](screen, the_world_is_a_happy_place)
-                pygame.display.flip()
-                clock.tick(fps)
-                
-                
+    screen.fill((255, 255, 255))
 
-def do_circle_demo(surface, counter):
-        x = surface.get_width() // 2
-        y = surface.get_height() // 2
-        max_radius = min(x, y) * 4 // 5
-        radius = abs(int(math.sin(counter * 3.14159 * 2 / 200) * max_radius)) + 1
-        color = (0, 140, 255) # aquamarine
-        
-        # Draw a circle
-        pygame.draw.circle(surface, color, (x, y), radius)
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 25)
 
+    pygame.display.update()
+    clock.tick(60)
 
-run_demos(400, 300, 60)
-
-# pygame.draw.circle(surface, color, (50, 50), 25)
+pygame.quit()
--- 7/music.py
+++ 7/music.py
@@ -1,66 +1,120 @@
 import pygame
-from random import shuffle
 
+pygame.init()
 
-def play_music(file_path):
+window_width = 800
+window_height = 600
+
+white = (255, 255, 255)
+
+# Images
+background_image = pygame.image.load('images/icons/bg1.png')
+play_icon = pygame.image.load('images/icons/play.png')
+pause_icon = pygame.image.load('images/icons/pause.png')
+next_icon = pygame.image.load('images/icons/next.png')
+previous_icon = pygame.image.load('images/icons/next.png')
+
+# Transforming
+background_image = pygame.transform.scale(background_image, (800, 800))
+play_icon = pygame.transform.scale(play_icon, (64, 64))
+pause_icon = pygame.transform.scale(pause_icon, (64, 64))
+next_icon = pygame.transform.scale(next_icon, (64, 64))
+previous_icon = pygame.transform.scale(previous_icon, (64, 64))
+previous_icon = pygame.transform.flip(previous_icon, True, False)
+
+game_display = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Music Player')
+
+font = pygame.font.SysFont(None, 36)
+
+def draw(icon, x, y):
+    game_display.blit(icon, (x, y))
+
+def music_player():
+    volume = 1
+    playing = False
+    paused = False
+    current_song_index = 0
+    songs = ["songs/Incepption_Time.mp3",
+            "songs/Experience.mp3",
+            "songs/Until_I_Found_You.mp3",
+            "songs/Detroit.mp3",
+            "songs/Interstellar.mp3"]  
+
     pygame.mixer.init()
-    pygame.mixer.music.load(f"./songs/{file_path}")
-    pygame.mixer.music.play(0)
+    pygame.mixer.music.load(songs[current_song_index])
 
-def stop_music():
-    pygame.mixer.music.stop()
+    panel_width = (next_icon.get_width() + play_icon.get_width() + previous_icon.get_width() + 50 * 2)
+    panel_x = (window_width - panel_width) // 2
 
-def create_background(width, height):
-    colors = [(255, 255, 255), (212, 212, 212)]
-    background = pygame.Surface((width, height))
-    tile_width = 20
-    y = 0
-    while y < height:
-            x = 0
-            while x < width:
-                    row = y // tile_width
-                    col = x // tile_width
-                    pygame.draw.rect(
-                            background, 
-                            colors[(row + col) % 2],
-                            pygame.Rect(x, y, tile_width, tile_width))
-                    x += tile_width
-            y += tile_width
-    return background
-
-
-def program(width, height):
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('press space to see next demo')
-    background = create_background(width, height)
-    song_paths = [
-        "Incepption_Time.mp3",
-        "Experience.mp3",
-        "Until_I_Found_You.mp3",
-        "Detroit.mp3",
-        "Interstellar.mp3"
-    ]
-    
-    current_song = 0
-    
-    while current_song < len(song_paths):
-        pressed_keys = pygame.key.get_pressed()
-        # command = input("Enter a command (play, stop, next, previous, quit): ").lower()
+    while True:
+        game_display.blit(background_image, (0, 0))
+        
+        draw(previous_icon, panel_x, 250)
+        draw(play_icon if not playing else pause_icon, panel_x + previous_icon.get_width() + 50, 250)
+        draw(next_icon, panel_x + previous_icon.get_width() + play_icon.get_width() + 100, 250)
+        
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    current_song = (current_song + 1) 
-                    # current_song = (current_song + 1) % len(file_paths)
-                    play_music(song_paths[current_song])
-                elif event.key == pygame.K_r:
-                    play_music(song_paths[current_song])
-                elif event.key == pygame.K_q or event.key == pygame.QUIT or pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT] and event.type == pygame.KEYDOWN and event.key == pygame.K_F4 :
-                    pygame.quit()
-                    return
+            pressed = pygame.key.get_pressed()
+            if event.type == pygame.QUIT or pressed[pygame.K_ESCAPE]:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if panel_x <= mouse_x <= panel_x + panel_width and 250 <= mouse_y <= 250 + play_icon.get_height():
+                    if panel_x <= mouse_x <= panel_x + previous_icon.get_width() and 250 <= mouse_y <= 250 + previous_icon.get_height():
+                        current_song_index = (current_song_index - 1) % len(songs)
+                        pygame.mixer.music.load(songs[current_song_index])
+                        pygame.mixer.music.play()
+                        playing = True
+                        paused = False
+                    elif panel_x + previous_icon.get_width() + 50 <= mouse_x <= panel_x + previous_icon.get_width() + play_icon.get_width() + 50 and 250 <= mouse_y <= 250 + play_icon.get_height():
+                        if playing:
+                            pygame.mixer.music.pause()
+                            paused = True
+                        else:
+                            pygame.mixer.music.unpause() if paused else pygame.mixer.music.play()
+                        playing = not playing
+                        paused = False
+                    elif (panel_x + previous_icon.get_width() + play_icon.get_width() + 100 <= mouse_x <= panel_x + panel_width and 250 <= mouse_y <= 250 + next_icon.get_height()):
+                        current_song_index = (current_song_index + 1) % len(songs)
+                        pygame.mixer.music.load(songs[current_song_index])
+                        pygame.mixer.music.play()
+                        playing = True
+                        paused = False
+            else:
+                pressed = pygame.key.get_pressed()
+                if pressed[pygame.K_RIGHT]:
+                    current_song_index = (current_song_index + 1) % len(songs)
+                    pygame.mixer.music.load(songs[current_song_index])
+                    pygame.mixer.music.play()
+                    playing = True
+                    paused = False
+                elif pressed[pygame.K_LEFT]:
+                    current_song_index = (current_song_index - 1) % len(songs)
+                    pygame.mixer.music.load(songs[current_song_index])
+                    pygame.mixer.music.play()
+                    playing = True
+                    paused = False
+                elif pressed[pygame.K_SPACE]:
+                    if playing:
+                        pygame.mixer.music.pause()
+                        paused = True
+                    else:
+                        pygame.mixer.music.unpause() if paused else pygame.mixer.music.play()
+                    playing = not playing
+                    paused = False
+                elif pressed[pygame.K_UP]:
+                    volume += 0.1  
+                    if volume > 1:
+                        volume = 1
+                    pygame.mixer.music.set_volume(volume)
+                elif pressed[pygame.K_DOWN]:
+                    volume -= 0.1 
+                    if volume < 0:
+                        volume = 0
+                    pygame.mixer.music.set_volume(volume) 
+                    
+        pygame.display.update()
 
-    pygame.quit()
-
-if __name__ == "__main__":
-    program(500, 500)
-
+music_player()
